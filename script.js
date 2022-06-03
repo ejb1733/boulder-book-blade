7e228ea2fc08512a24f100d0a19e6d2b9b4bcc15
ejb1733 <ethanjbillings@gmail.com>
2022-06-02 21:15:52 -0700

    Add interactive user ui
    - user can select rock, paper or scissors by clicking the respective button
    - game will play until 5 rounds are won or lost
    - user can click to restart the game after 5 wins/losses

--- script.js
+++ script.js
@@ -12,8 +12,20 @@ const options = [ROCK, PAPER, SCISSORS];
 
 var continuePlaying = true;
 
-// const button = document.querySelector('#rock');
-// button.addEventListener('click', updateButton);
+const results = document.querySelector('.results');
+const body = document.querySelector('body');
+
+let wins = 0;
+let losses = 0;
+
+let rockbtn = document.querySelector('#rock');
+let paperbtn = document.querySelector('#paper');
+let scissorsbtn = document.querySelector('#scissors');
+
+results.innerText = `
+
+WINS: ${wins} 
+LOSSES: ${losses}`;
 
 function computerPlay() {
     let random = Math.floor(Math.random() * 3);
@@ -56,47 +68,52 @@ function playRound(userSelection, computerSelection) {
         result = TIE;
     } else if (WIN_CONDITIONS.includes(currCombo)) {
         result = USER_WIN;
+        wins++;
     } else {
         result = USER_LOSE;
+        losses++;
     }
+    results.innerText = `Your ${userSelection} ${result} versus ${computerSelection} 
+
+    WINS: ${wins} 
+    LOSSES: ${losses}`;
+    checkGameState();
     console.log(`Your ${userSelection} ${result} versus ${computerSelection}!`);
     return result;
 }
 
-function updateButton() {
-    switch (true) {
-        case (button.value == ROCK):
-            playRound(ROCK, computerPlay);
-            break;
-        case (button.value == PAPER):
-            playRound(PAPER, computerPlay);
-            break;
-        case (button.value == SCISSORS):
-            playRound(PAPER, computerPlay);
-            break;
+var checkGameState = () => {
+    if (wins == 5 || losses == 5) {
+
+        btn = document.createElement('button');
+        btn.innerHTML = 'click to restart';
+        for (let i = 0; i < gbArray.length; i++) {
+            gbArray[i].remove();
+        }
+        btn.addEventListener('click', () => {
+            resetGame();
+            btn.remove();
+            body.prepend(rockbtn, paperbtn, scissorsbtn);
+        })
+        body.appendChild(btn);
     }
 }
 
-function game() {
-    let user_wins = 0;
-    let computer_wins = 0;
-    while (continuePlaying) {
-        let userSelection = prompt("What's it gonna be, punk?");
-        let result = playRound(userSelection, computerPlay());
-        if (result == USER_WIN) {
-            user_wins++;
-            console.log(`You've won ${user_wins} rounds!`);
-        } else if (result == USER_LOSE) {
-            computer_wins++;
-            console.log(`You've lost ${computer_wins} rounds!`);
-        }
-        if (user_wins == 5 || computer_wins == 5) {
-            continuePlaying = false;
-        }
-    }
+var resetGame = function() {
+    wins = 0;
+    losses = 0;
+    console.log('reset');
+    results.innerText = `
+
+WINS: ${wins} 
+LOSSES: ${losses}`;
 }
 
-game();
+let gbArray = Array.from(document.querySelectorAll('.gb'));
+for (let i = 0; i < 3; i++) {
+    gbArray[i].addEventListener('click', () => {
+        playRound(gbArray[i].id, computerPlay());
+    })
+}
 
 counter();
-console.log(playRound(ROCK, computerPlay()));
